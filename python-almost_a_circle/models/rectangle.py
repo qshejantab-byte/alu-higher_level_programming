e744b14bcd128e7d7c9eb4a1265ca5fe4f19b78d
qshejantab-byte <qshejantab@gmail.com>
2026-07-31 10:47:50 +0000

    8. Update #0, 9. Update #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -106,3 +106,25 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle via args (no-keyword) or kwargs (keyword).
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represents height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+
+        """
+        if args and len(args) > 0:
+            attrs = ["id", "width", "height", "x", "y"]
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
