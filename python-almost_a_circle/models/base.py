a85f450c5a38ec2ca557b35341ec203197021a02
qshejantab-byte <qshejantab@gmail.com>
2026-07-31 11:03:20 +0000

    19. File to instances

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -93,3 +93,19 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances from a JSON file.
+
+        The filename is <cls.__name__>.json. If the file doesn't exist,
+        returns an empty list.
+
+        """
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, "r") as f:
+                list_dicts = cls.from_json_string(f.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
