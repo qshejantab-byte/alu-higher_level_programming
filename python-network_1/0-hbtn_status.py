ad0165ad1297c1a336ef784071ea9b3490b656be
qshejantab-byte <your-email@example.com>
2026-07-31 09:04:13 +0000

    0. What's my status? #0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+
+with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as r:
+    body = r.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
