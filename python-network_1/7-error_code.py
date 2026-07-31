167039a0f1ebe7d17625fb427b738e0291be053b
qshejantab-byte <qshejantab@gmail.com>
2026-07-31 09:39:08 +0000

    6. POST an email #1, 7. Error code #1

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that displays the body of a response, checking for error codes."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
