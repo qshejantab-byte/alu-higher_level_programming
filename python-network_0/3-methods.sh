29f81faefbf4d5e502ae9fd7a7bba1e8c262f7b4
qshejantab-byte <qshejantab@gmail.com>
2026-08-01 14:21:16 +0000

    3. cURL only methods

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods a server accepts for a given URL
-curl -sI "$1" | grep -i "^allow:" | cut -d' ' -f2- | tr -d '\r'
+curl -sI -X OPTIONS "$1" | grep -i "^allow:" | cut -d' ' -f2- | tr -d '\r'
