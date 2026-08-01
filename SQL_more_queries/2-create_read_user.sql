edb0821d2001ec72f24ce1fa269c21f890a55b0c
qshejantab-byte <qshejantab@gmail.com>
2026-08-01 14:36:54 +0000

    2. Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,5 @@
--- Creates the database hbtn_0d_2, without failing if it already exists
+-- creates the database hbtn_0d_2 and user user_0d_2 with SELECT privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Creates the user user_0d_2, without failing if it already exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grants only SELECT privilege on hbtn_0d_2 to user_0d_2
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
